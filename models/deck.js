63294c172d45edacb0d7a4bfae8b5cf30bf12d9a
LuckiPhoenix <huynhchihen2005@gmail.com>
2025-03-29 16:51:45 +0700

    feat: add deck model

---
+++ models/deck.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const DeckSchema = new mongoose.Schema({
+    owner: { 
+        type: String,
+        required: true 
+    },
+    title: { 
+        type: String, 
+        required: true 
+    },
+    description: { 
+        type: String 
+    },
+    img: { 
+        type: String 
+    },
+    cards: [{ 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Dictionary' 
+    }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Deck', DeckSchema);
